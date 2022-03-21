875961384f9fef85367f9393863094f00c8d265b
mkurbonova <mkurbonova@outlook.com>
2022-03-20 22:20:22 -0400

    Your message here

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-let cheese = 'brie';
-cheese = 'parmesan';
-const firstname = 'Mokhsira';
-const lastname = 'Kurbonova';
-const fullname = firstname + '' + lastname;
-let age = 24;
-age += 2;
-const names = ['Sam', 'Mokhsira'];
-const person0bject = { name: 'Mokhsira', age: 24 };
+'use strict';
+console.log('working!');
 
-console.log(person0bject.age + 1);
+// 1. create a variable called firstName and assign it the value of your first name
+const firstname = 'Mokhsira';
+// 2. print firstName to the console
+console.log(firstname);
+// 3. Create a variable called lastName and assign it the value of your last name
+const lastname = 'Kurbonova';
+// 4. print lastName to the console
+console.log(lastname);
+// 5. create a variable called age and assign it the value of your age (or any number you like)
+let age = 24;
+// 6. print a string to the console that reads: 'My name is [first name] [last name] and my favourite number is [age]'
+console.log(
+  `My name is ${firstname} ${lastname} and my favourite number is ${age}`,
+);
+
+// 7. create a variable X and assign it any number
+let X = 3;
+// 8. create a variable Y and assign it any number
+let Y = 7;
+// 9. print to the console the total of X and Y
+console.log(X + Y);
+// 10. print to the console the total of X subtracted from Y
+console.log(Y - X);
+// 11. print to the console the total of X multiplied by Y
+console.log(X * Y);
+// 12. create a variable called changeNumber and assign it the number 5 and print it to the console
+let changeNumber = 5;
+console.log(changeNumber);
+// 13. re-assign the value of changeNumber to 300 and print it to the console
+changeNumber = 300;
+console.log(changeNumber);
+// 14. uncomment the variables below, concatonate them and print the result to the console
+
+const fruit = 'Oranges';
+const vegetable = 'potatos';
+const drink = 'cola';
+console.log(fruit + '' + vegetable + '' + drink);
