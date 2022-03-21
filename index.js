055362344f86c679534e4fee9ffa797b67fa333a
mkurbonova <mkurbonova@outlook.com>
2022-03-20 21:51:38 -0400

    Your message here

---
+++ index.js
@@ -0,0 +1,11 @@
+let cheese = 'brie';
+cheese = 'parmesan';
+const firstname = 'Mokhsira';
+const lastname = 'Kurbonova';
+const fullname = firstname + '' + lastname;
+let age = 24;
+age += 2;
+const names = ['Sam', 'Mokhsira'];
+const person0bject = { name: 'Mokhsira', age: 24 };
+
+console.log(person0bject.age + 1);
